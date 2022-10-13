0f823e91551701b199247a584006f023aed5b64a
Christoph Wallner <wallner.christoph@t-online.de>
2022-10-13 04:11:36 +0200

    added slashcommands loader and some example slashcommands

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ require("dotenv").config()
 
 const generateImage = require("./generateImage")
 
-var answers = ["soos", "fiif", "mööm", "saas", "smens", "yoy", "coc", "lool"]
-
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -18,6 +16,8 @@ const client = new Client({
 
 client.login(process.env.TOKEN)
 
+
+var answers = ["soos", "fiif", "mööm", "saas", "smens", "yoy", "coc", "lool"]
 client.on("messageCreate", (message) => {
     if (message.content == "smums" || 
             message.content == "fiif" || 
@@ -51,11 +51,28 @@ let bot = {
 
 client.commands = new Discord.Collection()
 client.events = new Discord.Collection()
+client.slashcommands = new Discord.Collection()
 
 client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
 client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
+client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
 
 client.loadEvents(bot, false)
 client.loadCommands(bot, false)
+client.loadSlashCommands(bot, false)
+
+client.on("interactionCreate", (interaction) => {
+    if (!interaction.isCommand()) return 
+    if (!interaction.inGuild()) return interaction.reply("This command can only be used in a server")
+
+    const slashcmd = client.slashcommands.get(interaction.commandName)
+
+    if (!slashcmd) return interaction.reply("Invalid slash command")
+
+    if (slashcmd.perm && !interaction.member.permissions.has(slashcmd.perm))
+        return interaction.reply("You do not have permission for this command")
+
+    slashcmd.run(client, interaction)
+})
 
 module.exports = bot
---
+++ loadslash.js
@@ -0,0 +1,39 @@
+const {Client, GatewayIntentBits} = require("discord.js")
+const Discord = require("discord.js")
+
+require("dotenv").config()
+
+const client = new Discord.Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildInvites
+    ]
+})
+
+let bot = {
+    client
+}
+
+
+
+const guildId = "327553475833495554"
+
+client.slashcommands = new Discord.Collection() 
+
+client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
+client.loadSlashCommands(bot, false)
+
+client.on("ready", async () => {
+    const guild = client.guilds.cache.get(guildId)
+    if (!guild)
+        return console.error("Target guild not found")
+    
+    await guild.commands.set([...client.slashcommands.values()])
+    console.log(`Successfully loaded in ${client.slashcommands.size}`)
+    process.exit(0)
+})
+
+client.login(process.env.TOKEN)
